097f41bacf2b3ccb24adf267e28fcc3c212a4c0f
Simran145678 <hkaur607@my.centennialcollege.ca>
2020-10-25 21:36:55 -0400

    user.js added to models

--- server/config/app.js
+++ server/config/app.js
@@ -5,6 +5,13 @@ let path = require("path");
 let cookieParser = require("cookie-parser");
 let logger = require("morgan");
 
+//modules for authentication
+let session = require("express-session");
+let passport = require("passport");
+let passportLocal = require("passport-local");
+let localstrategy = passportLocal.Strategy;
+let flash = require("connect-flash");
+
 //database setup
 let mongoose = require("mongoose");
 let DB = require("./db");
@@ -21,6 +28,7 @@ mongoDB.once("open", () => {
 let indexRouter = require("../routes/index");
 let usersRouter = require("../routes/users");
 let contactsRouter = require("../routes/contact");
+const { Passport } = require("passport");
 
 let app = express();
 
@@ -34,6 +42,22 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "../../public")));
 app.use(express.static(path.join(__dirname, "../../node_modules")));
+
+//setup express session
+app.use(
+  session({
+    secret: "SomeSecret",
+    saveUninitialized: false,
+    resave: false,
+  })
+);
+
+//initialize flash
+app.use(flash);
+
+//initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
